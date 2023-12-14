b576f1c3eaf2dd889b3c0b98469f225e619ab92d
malithakabir <malitha12345@gmail.com>
2023-12-15 05:11:33 +0600

    update filenames

--- src/loader.py
+++ src/loader.py
@@ -2,10 +2,13 @@ import os
 import numpy as np
 import pandas as pd
 import re
-
+import json
 # import talib as tb
 from sklearn.preprocessing import StandardScaler
-import pandas_datareader as pdr
+from pandas_datareader import data as pdr
+from datetime import datetime
+import yfinance as yf
+yf.pdr_override()
 
 
 class DataLoader:
@@ -23,27 +26,50 @@ class DataLoader:
         self.df_processed = None
         self.filename_raw = None
         self.df_raw = None
+        
+        with open('registration.json', encoding='UTF-8') as f:
+            self.registraion = json.load(f)
+
+        self.dateformat = '%Y-%m-%d'
+        self.remote_data = None
 
     def set_datadir(self, dirname):
         self.datadir = dirname
 
-    def read_local(self, isTechnical=True):
+    def read_local(self, isTechnical=True, latest=True):
         self.isTechnical = isTechnical
+        rdata = self.registration.get(self.ticker, None).get('data', None)
+        if latest:
+            key = str(max([int(i) for i in rdata.keys()]))
+        else:
+            key = "0" 
+        
         if isTechnical:
-            fname = "{}{}_TA_V1.csv".format(self.datadir, self.ticker)
-            print("Reading from: {}".format(fname))
-            self.filename_processed = fname
+            fname = rdata.get(key, None).get('technical_analysis_filename', None)
+            # fname = "{}{}_TA_V1.csv".format(self.datadir, self.ticker)
+            fpath = self.datadir + fname
+            print("Reading from: {}".format(fpath))
+            self.filename_processed = fpath
             self.df_processed = pd.read_csv(
                 self.filename_processed, parse_dates=True, index_col=0
             )
         else:
-            fname = "{}{}_RAW.csv".format(self.datadir, self.ticker)
-            print("Reading from: {}".format(fname))
-            self.filename_raw = fname
+            fname = rdata.get(key, None).get('raw_data_filename', None)
+            # fname = "{}{}_RAW_V1.csv".format(self.datadir, self.ticker
+            fpath = self.datadir + fname
+            print("Reading from: {}".format(fpath))
+            self.filename_raw = fpath
             self.df_raw = pd.read_csv(
                 self.filename_raw, header=0, index_col="Date", parse_dates=True
             )
-
+    def read_remote(self, until, since='2006-12-13'):
+        startdate = since
+        enddate = datetime.strptime(until, self.dateformat)
+        self.remote_data = pdr.get_data_yahoo([self.ticker], start=startdate, end=enddate)
+    
+    def save_remote_data(self):
+        fpath = ''
+        print('saving data to {}'.format(fpath))
     #     # if tf == None:
     #     #     self.df = data
     #     # else:
@@ -69,6 +95,9 @@ class ModelLoder:
         self.forecast_horizon = None
         self.rolling_window = None
         self.model = None
+        
+        with open('registration.json', encoding='UTF-8') as f:
+            self.registraion = json.load(f)
 
     def set_model_dir(self, dirname):
         self.modeldir = dirname
@@ -82,12 +111,21 @@ class ModelLoder:
     def set_rolling_window(self, val):
         self.rolling_window = val
 
-    def read_model_local(self):
-        model_name = "{}LSTM_{}_RW{}_FH{}.h5".format(
-            self.modeldir, self.ticker, self.rolling_window, self.forecast_horizon
-        )
-        print("Loading model from {}".format(model_name))
-        self.model = load_model(model_name)
+    def read_model_local(self, latest=True):
+        rdata = self.registration.get(self.ticker, None).get('model', None)
+        if latest:
+            key = str(max([int(i) for i in rdata.keys()]))
+        else:
+            key = "0"
+        model_name = rdata.get(key, None).get('model_name', None)
+        model_path = self.modeldir + model_name
+        
+        # model_name = "{}LSTM_{}_RW{}_FH{}_V1.h5".format(
+        #     self.modeldir, self.ticker, self.rolling_window, self.forecast_horizon
+        # )
+        
+        print("Loading model from {}".format(model_path))
+        self.model = load_model(model_path)
 
     def get_model_list(self):
         """
